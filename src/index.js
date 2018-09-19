eadaeda2f4cf56aeacf106fa2be188282a0305b6
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-09-20 00:49:04 +0430

    feat: sass file just imported to the project.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
+import './styles/index.scss';
+
 const root = document.getElementById("root");
 ReactDOM.render(<App/>, root);
