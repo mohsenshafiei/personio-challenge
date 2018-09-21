dd2bf35ec89ac659e7acecbe938a431f4fbe4f55
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-09-21 18:04:48 +0430

    fix: some minor refactoring in the code and title just removed from hierarchy page.

--- src/ui/pages/Hierarchy.jsx
+++ src/ui/pages/Hierarchy.jsx
@@ -6,113 +6,140 @@ class Hierarchy extends React.Component {
     super(props);
     this.state = {
       title: 'Personio Company Hierarchy',
-      colors: [
-        'Red',
-        'Green',
-        'Blue',
-        'Yellow',
-        'Black',
-        'White',
-        'Orange',
-      ],
-      employees: [
-        {
-          Sophie: {
-            position: 'VP Engineering',
-            employees: [{
-              Nick: {
-                position: 'Team Lead',
+      employees: [{
+        Arsenie: {
+          position: 'CTO',
+          employees: [
+            {
+              Sophie: {
+                position: 'VP Engineering',
+                employees: [{
+                  Nick: {
+                    position: 'Team Lead',
+                    employees: [
+                      {
+                        Pete: {
+                          position: 'Backend Engineer',
+                          employees: [],
+                        },
+                      },
+                      {
+                        Barbara: {
+                          position: 'Frontend Engineer',
+                          employees: [],
+                        },
+                      },
+                    ],
+                  },
+                }],
+              },
+            },
+            {
+              Sophie: {
+                position: 'VP Engineering',
+                employees: [{
+                  Nick: {
+                    position: 'Team Lead',
+                    employees: [
+                      {
+                        Pete: {
+                          position: 'Backend Engineer',
+                          employees: [],
+                        },
+                      },
+                      {
+                        Barbara: {
+                          position: 'Frontend Engineer',
+                          employees: [],
+                        },
+                      },
+                    ],
+                  },
+                }],
+              },
+            },
+            {
+              Sophie: {
+                position: 'VP Engineering',
+                employees: [{
+                  Nick: {
+                    position: 'Team Lead',
+                    employees: [
+                      {
+                        Pete: {
+                          position: 'Backend Engineer',
+                          employees: [],
+                        },
+                      },
+                      {
+                        Barbara: {
+                          position: 'Frontend Engineer',
+                          employees: [],
+                        },
+                      },
+                    ],
+                  },
+                }],
+              },
+            },
+            {
+              Iulian: {
+                position: 'Engineering Manager',
+                employees: [{
+                  Rebeca: {
+                    position: 'Team Lead',
+                    employees: [
+                      {
+                        Chris: {
+                          position: 'Backend Engineer',
+                          employees: [],
+                        },
+                      },
+                      {
+                        Joe: {
+                          position: 'Frontend Engineer',
+                          employees: [],
+                        },
+                      },
+                    ],
+                  },
+                }],
+              },
+            },
+            {
+              Robert: {
+                position: 'Engineering Manager',
                 employees: [
                   {
-                    Pete: {
-                      position: 'Backend Engineer',
-                      employees: [],
+                    Susan: {
+                      position: 'Team Lead',
+                      employees: [
+                        {
+                          Alex: {
+                            position: 'Backend Lead',
+                            employees: [],
+                          },
+                        },
+                        {
+                          Monica: {
+                            position: 'Frontend Lead',
+                            employees: [],
+                          },
+                        },
+                      ],
                     },
-                  },
-                  {
-                    Barbara: {
-                      position: 'Frontend Engineer',
-                      employees: [],
-                    },
-                  },
-                ],
+                  }],
               },
-            }],
-          },
+            },
+          ],
         },
-        {
-          Iulian: {
-            position: 'Engineering Manager',
-            employees: [{
-              Rebeca: {
-                position: 'Team Lead',
-                employees: [
-                  {
-                    Chris: {
-                      position: 'Backend Engineer',
-                      employees: [],
-                    },
-                  },
-                  {
-                    Joe: {
-                      position: 'Frontend Engineer',
-                      employees: [],
-                    },
-                  },
-                ],
-              },
-            }],
-          },
-        },
-        {
-          Robert: {
-            position: 'Engineering Manager',
-            employees: [
-              {
-                Susan: {
-                  position: 'Team Lead',
-                  employees: [
-                    {
-                      Alex: {
-                        position: 'Backend Lead',
-                        employees: [],
-                      },
-                    },
-                    {
-                      Monica: {
-                        position: 'Frontend Lead',
-                        employees: [],
-                      },
-                    },
-                  ],
-                },
-              }],
-          },
-        },
-      ],
-      employeesFlat: {
-        Sophie: {
-          position: 'VP Engineering',
-          employees: {},
-        },
-        Iulian: {
-          position: 'Engineering Manager',
-          employees: {},
-        },
-        Robert: {
-          position: 'Engineering Manager',
-          employees: {},
-        },
-      },
+      }],
     };
   }
 
   render() {
     return (
       <div className="hierarchy">
-        <div className="title">
-          <h4>{ this.state.title }</h4>
-        </div>
         <div className="list" >
           <List employees={this.state.employees}/>
         </div>
