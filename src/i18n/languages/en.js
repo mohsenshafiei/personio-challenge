63d8ca0fdda597ca4f2ed95c5f06ab919f115324
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2018-09-21 21:33:25 +0430

    feat: links just added to i18n en and de json file.

--- src/i18n/languages/en.js
+++ src/i18n/languages/en.js
@@ -2,9 +2,14 @@ export default {
   translation: {
     language: 'De',
     title: 'Personio',
+    welcome: 'Welcome to Personio Challenge',
+    motto: 'Enabling Better Organizations',
     navigation: {
       home: 'Home',
       hierarchy: 'Hierarchy',
+    },
+    links: {
+      hierarchy: 'Get The Hierarchy',
     },
   },
 };
