b67c7ad6a502e73e454eda5616070d1d3980a26e
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-23 11:21:58 +0330

    feat: store just added temporarily to the home page and hierachy as a prop but it should remove after connect.

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ import Home from '../ui/pages/Home.jsx';
 import Hierarchy from '../ui/pages/Hierarchy.jsx';
 import Error from '../ui/pages/Error.jsx';
 
-const Router = ({ location }) => (
+const Router = ({ location, store }) => (
   <div className="router">
   <TransitionGroup className="transition-group">
     <CSSTransition key={location.key} classNames={'fade'} timeout={{ enter: 300, exit: 300 }}>
       <section className="route-section">
         <Switch location={location}>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/hierarchy" component={Hierarchy} />
+          <Route exact path="/" render={() => <Home store={store}/>} />
+          <Route exact path="/hierarchy" render={() => <Hierarchy store={store.hierarchy} />} />
           <Route render={() => <Error error={404}/>} />
         </Switch>
       </section>
@@ -27,6 +27,7 @@ Router.propTypes = {
   location: PropTypes.shape({
     location: PropTypes.object,
   }),
+  store: PropTypes.object,
 };
 
 export default withRouter(Router);
