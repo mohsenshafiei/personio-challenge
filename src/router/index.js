9364bc20d6e4739e8f5688495b1446f77ce53ffe
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-23 16:10:09 +0330

    refactor: language prop just removed from the router.

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ import Home from '../ui/pages/Home.jsx';
 import Hierarchy from '../ui/pages/Hierarchy.jsx';
 import Error from '../ui/pages/Error.jsx';
 
-const Router = ({ location, store }) => (
+const Router = ({ location }) => (
   <div className="router">
   <TransitionGroup className="transition-group">
     <CSSTransition key={location.key} classNames={'fade'} timeout={{ enter: 300, exit: 300 }}>
       <section className="route-section">
         <Switch location={location}>
-          <Route exact path="/" render={() => <Home store={store}/>} />
-          <Route exact path="/hierarchy" render={() => <Hierarchy store={store.hierarchy} />} />
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/hierarchy" component={Hierarchy} />
           <Route render={() => <Error error={404}/>} />
         </Switch>
       </section>
@@ -27,7 +27,6 @@ Router.propTypes = {
   location: PropTypes.shape({
     location: PropTypes.object,
   }),
-  store: PropTypes.object,
 };
 
 export default withRouter(Router);
