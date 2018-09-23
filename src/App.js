40299e9a28659c90bb4674d2992f5bf694414ffa
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-23 15:52:56 +0330

    feat: change language just added to the App.js but it needs some minor fixes.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import i18n from './i18n';
 import Router from './router';
 import Header from './ui/components/Header.jsx';
 import store from './store';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: i18n.language,
+    };
+  }
+
+  componentDidMount() {
+    i18n.on('languageChanged', () => {
+      this.setState({
+        language: i18n.language,
+      });
+    });
+  }
+
   render() {
     return (
       <Provider store={store} >
         <div>
           <Header />
-          <Router store={store}/>
+          <Router />
         </div>
       </Provider>
     );
