d8bd63a278eff9fc43470f5329b3151dd3d8ed3f
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-23 11:24:24 +0330

    feat: jsonUploadFile action just added to the Home page.

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
 import i18n from '../../i18n';
 import Uploader from '../components/Uploader.jsx';
 import Personio from '../components/Personio.jsx';
+
+import jsonFileUploaded from '../../store/hierarchy/actions';
+
 
 class Home extends React.Component {
   constructor(props) {
@@ -14,6 +17,10 @@ class Home extends React.Component {
   }
 
   render() {
+    const {
+      store,
+    } = this.props;
+
     return (<div className="home">
       <div className="logo">
         <Personio/>
@@ -24,7 +31,12 @@ class Home extends React.Component {
       {!this.state.uploaded
         && <Uploader
           classname="upload"
-          onSelect={ (e) => { console.log(JSON.parse(e)); this.setState({ uploaded: true }); } }/>}
+          onSelect={ (e) => {
+            this.setState({ uploaded: true });
+            store.dispatch(jsonFileUploaded(JSON.parse(e)));
+          }
+          }
+      />}
       {this.state.uploaded
         && <div className="links">
         <Link className="links__item" to='/hierarchy'>{i18n.t('links.hierarchy')}</Link>
@@ -32,5 +44,9 @@ class Home extends React.Component {
     </div>);
   }
 }
+
+Home.propTypes = {
+  store: PropTypes.object,
+};
 
 export default Home;
