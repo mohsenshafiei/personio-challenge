35bde7c1647194241fdfc5cf9625cfd0ac4ae1e0
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-23 16:15:03 +0330

    feat: mainSaga just imported into the Sagas.

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,9 +1,11 @@
 import { all } from 'redux-saga/effects';
 import hierarchySaga from './hierarchy/saga';
+import mainSaga from './main/saga';
 
 function* sagas() {
   yield all([
     ...hierarchySaga,
+    ...mainSaga,
   ]);
 }
 
