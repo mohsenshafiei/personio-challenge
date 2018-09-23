515431fe7bb173f9d89766301e5523feffec9a94
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-23 11:28:28 +0330

    feat: store just initialized and middleware and enhancer just added to the store.

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef,no-underscore-dangle */
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers,
+  compose,
+} from 'redux';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import hierarchyReducers from './hierarchy/reducers';
+import sagas from './sagas';
+
+const reducers = combineReducers({
+  hierarchy: hierarchyReducers,
+});
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = typeof window === 'object'
+&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const middleware = applyMiddleware(logger, sagaMiddleware);
+const enhancer = composeEnhancers(middleware);
+const store = createStore(reducers, enhancer);
+
+sagaMiddleware.run(sagas);
+export default store;
