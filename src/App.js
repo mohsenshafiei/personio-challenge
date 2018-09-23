73bbc0235b24a6ebb66e7d24635fc3131b440539
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-23 11:01:03 +0330

    feat: Provider just added to the App.js.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Router from './router';
-
 import Header from './ui/components/Header.jsx';
+import store from './store';
 
-const App = () => (
-    <div>
-      <Header/>
-      <Router/>
-    </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store} >
+        <div>
+          <Header />
+          <Router store={store}/>
+        </div>
+      </Provider>
+    );
+  }
+}
 
 export default App;
