e46317f4b1958b0f2324d6931b13ab9293212094
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-25 00:36:06 +0330

    feat: some minor refactoing and hierachyChanged Just added to our saga to handle different hierachy changes.

--- src/store/hierarchy/saga.js
+++ src/store/hierarchy/saga.js
@@ -1,11 +1,39 @@
 import {
-  put, takeEvery,
+  put, takeEvery, select,
 } from 'redux-saga/effects';
 
-function* jsonFileUploaded() {
+const getPerson = (employees = [], personId) => {
+  let employee = employees.find(person => person.id === personId);
+  if (employee) {
+    return employee;
+  }
+  for (let i = 0; i < employees.length; i += 1) {
+    employee = getPerson(employees[i].employees, personId);
+    if (employee) {
+      return employee;
+    }
+  }
+  return undefined;
+};
+
+function* hierarchyChanged(action) {
+  if (action.leaderId.indexOf(action.personId) === 0) {
+    alert('This Action Is Not Possible, You Are Making a Loop!');
+  } else {
+    const person = yield select(state => getPerson(state.hierarchy.employees, action.personId));
+    if (person) {
+      yield put({ type: 'REMOVE_PERSON', personId: action.personId });
+      yield put({ type: 'ADD_PERSON', leaderId: action.leaderId, person });
+    }
+  }
+}
+
+function* fileUploaded() {
+  // you can send it to server here
   yield put({ type: 'JSON_FILE_UPLOADED_SUCCESSFUL', payload: true });
 }
 
 export default [
-  takeEvery('JSON_FILE_UPLOADED', jsonFileUploaded),
+  takeEvery('JSON_FILE_UPLOADED', fileUploaded),
+  takeEvery('CHANGE_HIERARCHY', hierarchyChanged),
 ];
