315414cf1fa943b8a9df540bc1111dc84a474f16
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-25 19:17:45 +0330

    feat: i18n just added to filter component options.

--- src/ui/pages/Hierarchy.jsx
+++ src/ui/pages/Hierarchy.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import List from '../components/List.jsx';
 import Filter from '../components/Filter.jsx';
+import i18n from '../../i18n';
 
 class Hierarchy extends React.Component {
   constructor(props) {
@@ -14,10 +15,10 @@ class Hierarchy extends React.Component {
     return (
       <div className="hierarchy">
         <Filter
-          title="Filter Items"
+          title={i18n.t('filter.title')}
           items={[
-            { title: 'Names', id: 1 },
-            { title: 'Positions', id: 2 },
+            { title: i18n.t('filter.name'), id: 1 },
+            { title: i18n.t('filter.position'), id: 2 },
           ]}
           onSelect={(id) => {
             console.log(id);
