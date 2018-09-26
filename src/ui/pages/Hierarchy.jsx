2518d39bc8222ebfcb6822856b7a3a8e55eac8a1
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-26 11:44:39 +0330

    feat: filter action just added to Hierachy page to handle filterration on our list.

--- src/ui/pages/Hierarchy.jsx
+++ src/ui/pages/Hierarchy.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import List from '../components/List.jsx';
 import Filter from '../components/Filter.jsx';
 import i18n from '../../i18n';
+
+import { changeFilter } from '../../store/hierarchy/actions';
 
 class Hierarchy extends React.Component {
   constructor(props) {
@@ -17,13 +21,14 @@ class Hierarchy extends React.Component {
         <Filter
           title={i18n.t('filter.title')}
           items={[
+            { title: i18n.t('filter.all'), id: 0 },
             { title: i18n.t('filter.name'), id: 1 },
             { title: i18n.t('filter.position'), id: 2 },
           ]}
-          onSelect={(id) => {
-            console.log(id);
+          onSelect={(filter) => {
+            this.props.changeFilter(filter.title);
           }}
-          display="inline"
+          display="inline-block"
         />
         <div className="list" >
           <List />
@@ -33,4 +38,14 @@ class Hierarchy extends React.Component {
   }
 }
 
-export default Hierarchy;
+const mapDispatchToProps = dispatch => ({
+  changeFilter: (filter) => {
+    dispatch(changeFilter(filter));
+  },
+});
+
+Hierarchy.propTypes = {
+  changeFilter: PropTypes.func,
+};
+
+export default connect(null, mapDispatchToProps)(Hierarchy);
