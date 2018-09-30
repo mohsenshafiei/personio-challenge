429a628d58e7c4b45a669a8af734d7b8c4f04933
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-30 21:42:27 +0330

    refactor: action types just added to our action creators.

--- src/store/hierarchy/actions.js
+++ src/store/hierarchy/actions.js
@@ -1,37 +1,46 @@
+import {
+  JSON_FILE_UPLOADED,
+  CHANGE_HIERARCHY,
+  CHANGE_FILTER,
+  TOGGLE_COLLAPSE,
+  DETECT_MULTIPLE_BOSS,
+  REMOVE_MULTIPLE_PERSON,
+} from './actionTypes';
+
 export const fileUpload = payload => (
   {
-    type: 'JSON_FILE_UPLOADED',
+    type: JSON_FILE_UPLOADED,
     payload,
   }
 );
 
 export const changeHierarchy = (personId, leaderId) => (
   {
-    type: 'CHANGE_HIERARCHY',
+    type: CHANGE_HIERARCHY,
     personId,
     leaderId,
   }
 );
 export const changeFilter = filter => (
   {
-    type: 'CHANGE_FILTER',
+    type: CHANGE_FILTER,
     filter,
   }
 );
 export const toggleCollapse = personId => (
   {
-    type: 'TOGGLE_COLLAPSE',
+    type: TOGGLE_COLLAPSE,
     personId,
   }
 );
 export const detectMultipleBoss = () => (
   {
-    type: 'DETECT_MULTIPLE_BOSS',
+    type: DETECT_MULTIPLE_BOSS,
   }
 );
 export const removePerson = personId => (
   {
-    type: 'REMOVE_MULTIPLE_PERSON',
+    type: REMOVE_MULTIPLE_PERSON,
     personId,
   }
 );
