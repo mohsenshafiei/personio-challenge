827240318f801a82c58917d6685c24a6239cfb6d
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-09-30 22:24:29 +0330

    fix: action types resolve just fixed.

--- src/store/hierarchy/actions.js
+++ src/store/hierarchy/actions.js
@@ -1,46 +1,39 @@
-import {
-  JSON_FILE_UPLOADED,
-  CHANGE_HIERARCHY,
-  CHANGE_FILTER,
-  TOGGLE_COLLAPSE,
-  DETECT_MULTIPLE_BOSS,
-  REMOVE_MULTIPLE_PERSON,
-} from './actionTypes';
+import actionTypes from './actionTypes';
 
 export const fileUpload = payload => (
   {
-    type: JSON_FILE_UPLOADED,
+    type: actionTypes.JSON_FILE_UPLOADED,
     payload,
   }
 );
 
 export const changeHierarchy = (personId, leaderId) => (
   {
-    type: CHANGE_HIERARCHY,
+    type: actionTypes.CHANGE_HIERARCHY,
     personId,
     leaderId,
   }
 );
 export const changeFilter = filter => (
   {
-    type: CHANGE_FILTER,
+    type: actionTypes.CHANGE_FILTER,
     filter,
   }
 );
 export const toggleCollapse = personId => (
   {
-    type: TOGGLE_COLLAPSE,
+    type: actionTypes.TOGGLE_COLLAPSE,
     personId,
   }
 );
 export const detectMultipleBoss = () => (
   {
-    type: DETECT_MULTIPLE_BOSS,
+    type: actionTypes.DETECT_MULTIPLE_BOSS,
   }
 );
 export const removePerson = personId => (
   {
-    type: REMOVE_MULTIPLE_PERSON,
+    type: actionTypes.REMOVE_MULTIPLE_PERSON,
     personId,
   }
 );
