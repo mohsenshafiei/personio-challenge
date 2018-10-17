65801251ba395375d48d446a96a6d3a6c2fa0fb4
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-10-18 01:26:55 +0330

    refactor: just some minor cleaning up on router.

--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,23 @@ import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
-
 import Home from '../ui/pages/Home.jsx';
 import Hierarchy from '../ui/pages/Hierarchy.jsx';
 import Error from '../ui/pages/Error.jsx';
 
 const Router = ({ location }) => (
   <div className="router">
-  <TransitionGroup className="transition-group">
-    <CSSTransition key={location.key} classNames={'fade'} timeout={{ enter: 300, exit: 300 }}>
-      <section className="route-section">
-        <Switch location={location}>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/hierarchy" component={Hierarchy} />
-          <Route render={() => <Error error={404}/>} />
-        </Switch>
-      </section>
-    </CSSTransition>
-  </TransitionGroup>
+    <TransitionGroup className="transition-group">
+      <CSSTransition key={location.key} classNames={'fade'} timeout={{ enter: 300, exit: 300 }}>
+        <section className="route-section">
+          <Switch location={location}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/hierarchy" component={Hierarchy} />
+            <Route render={() => <Error error={404} />} />
+          </Switch>
+        </section>
+      </CSSTransition>
+    </TransitionGroup>
   </div>
 );
 
