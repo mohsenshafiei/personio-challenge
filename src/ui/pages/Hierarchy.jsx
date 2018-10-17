9f8211cc4631cce481ba1b8ca77e713428e2ee3f
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-10-17 11:04:06 +0330

    refactor: hierarchy component just class component to functional component.

--- src/ui/pages/Hierarchy.jsx
+++ src/ui/pages/Hierarchy.jsx
@@ -4,43 +4,31 @@ import PropTypes from 'prop-types';
 import List from '../components/List.jsx';
 import Filter from '../components/Filter.jsx';
 import i18n from '../../i18n';
-
 import { changeFilter, detectMultipleBoss } from '../../store/hierarchy/actions';
 
-class Hierarchy extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: 'Personio Company Hierarchy',
-    };
-  }
-
-  render() {
-    return (
-      <div className="hierarchy">
-        <Filter
-          title={i18n.t('filter.title')}
-          items={[
-            { title: i18n.t('filter.all'), id: 0 },
-            { title: i18n.t('filter.name'), id: 1 },
-            { title: i18n.t('filter.position'), id: 2 },
-            { title: i18n.t('filter.multipleBoss'), id: 3 },
-          ]}
-          onSelect={(filter) => {
-            this.props.changeFilter(filter.id);
-            if (filter.id === 3) {
-              this.props.detectMultipleBoss();
-            }
-          }}
-          display="inline-block"
-        />
-        <div className="list">
-          <List language={i18n.language} />
-        </div>
+const Hierarchy = props => (
+    <div className="hierarchy">
+      <Filter
+        title={i18n.t('filter.title')}
+        items={[
+          { title: i18n.t('filter.all'), id: 0 },
+          { title: i18n.t('filter.name'), id: 1 },
+          { title: i18n.t('filter.position'), id: 2 },
+          { title: i18n.t('filter.multipleBoss'), id: 3 },
+        ]}
+        onSelect={(filter) => {
+          props.changeFilter(filter.id);
+          if (filter.id === 3) {
+            props.detectMultipleBoss();
+          }
+        }}
+        display="inline-block"
+      />
+      <div className="list">
+        <List language={i18n.language} />
       </div>
-    );
-  }
-}
+    </div>
+);
 
 const mapDispatchToProps = dispatch => ({
   changeFilter: (filter) => {
