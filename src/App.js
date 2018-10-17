0b0c607f802c234ba27d3a9b99067783f16fee70
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2018-10-18 01:27:22 +0330

    refactor: just some minor cleaning up on App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import i18n from './i18n';
-import ErrorBoundary from './ui/components/ErrorBoundaryComponent.jsx';
+import ErrorBoundaryHandler from './ui/components/ErrorBoundaryHandler.jsx';
 import Router from './router';
 import Header from './ui/components/Header.jsx';
 import Notification from './ui/components/Notification.jsx';
 import store from './store';
-
 
 class App extends React.Component {
   constructor(props) {
@@ -26,15 +25,15 @@ class App extends React.Component {
 
   render() {
     return (
-      <ErrorBoundary>
-      <Provider store={store} >
-        <div>
-          <Header />
-          <Router />
-          <Notification />
-        </div>
-      </Provider>
-      </ErrorBoundary>
+      <ErrorBoundaryHandler>
+        <Provider store={store}>
+          <React.Fragment>
+            <Header />
+            <Router />
+            <Notification />
+          </React.Fragment>
+        </Provider>
+      </ErrorBoundaryHandler>
     );
   }
 }
